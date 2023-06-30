fb8d66d3b5916f760f17775514980fb7b345f1f4
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-30 14:23:53 +0900

    230630
    230630

--- js/custom.js
+++ js/custom.js
@@ -3,22 +3,43 @@ const MainSlide = new Swiper('.main_slide', {
 });
 
 const leftArrow = document.querySelector('#MainVisual .arrows .left');
-leftArrow.addEventListener('click', function(){
+leftArrow.addEventListener('click', function () {
     MainSlide.slidePrev();
 });
 const rightArrow = document.querySelector('#MainVisual .arrows .right');
-rightArrow.addEventListener('click', function(){
+rightArrow.addEventListener('click', function () {
     MainSlide.slideNext();
 });
 
 const proSlide = new Swiper('.pro_slide', {
     loop: true,
     // 3개이상부터 루프안돌아
-    slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
         el: ".sbar",
         type: "progressbar",
         clickable: true,
-      },
+    },
+
+    breakpoints: {
+        540: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+const mobileBtn = document.querySelector('.mobile_btn');
+const GNB = document.querySelector('#gnb');
+const H1 = document.querySelector('h1');
+
+mobileBtn.addEventListener('click', function () {
+    GNB.classList.toggle('on');
+    H1.classList.toggle('on');
+});
+
+GNB.addEventListener('wheel', function (e) {
+    if (GNB.classList.contains('on')) {
+        e.preventDefault();
+    }
 });
